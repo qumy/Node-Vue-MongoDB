f0d6d3f4d074c3b3a9ecad2a145ff39747012d96
jsusilo <enr@bk.ru>
2018-07-03 17:13:19 +0500

    Edited a user model

--- BudgetManagerAPI/app/models/user.js
+++ BudgetManagerAPI/app/models/user.js
@@ -1,2 +1,41 @@
 const mongoose = require('mongoose'),
   bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  clients: [{}]
+});
+
+// Здесь не будем пользоваться стрелочными функциями из-за автоматической привязки к лексической области видимости
+Schema.pre('save', function (next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, (error, salt) => {
+    if (error) return next(error);
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+      user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+Schema.methods.comparePassword = function (password, callback) {
+  bcrypt.compare(password, this.password, (error, matches) => {
+    if (error) return callback(error);
+    callback(null, matches);
+  });
+};
+
+mongoose.model('User', Schema);
